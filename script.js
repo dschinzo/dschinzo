7b0a9d9e7845727488c6cc0fff665a81f4f589b7
dschinzo <ds.chinzo@gmail.com>
2020-09-12 20:48:42 +0800

    done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Its working')
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-	setTheme('light')
+	setTheme('blue')
 }else{
 	setTheme(theme)
 }
@@ -21,11 +21,11 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = 'light.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = 'default.css'
 	}
 
 	if(mode == 'purple'){
